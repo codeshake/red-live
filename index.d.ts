762a0e41757f59981941e6a49cee0fc5ec5c996c
Sergey Lutov <doublemakers@gmail.com>
2021-06-13 17:06:48 +0300

    fix(types): move consts out of module

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,15 @@
+interface IRedLiveCallbacksStore {
+    [key: string]: Array<Function>
+}
+
+declare const redLiveCallbacksStoreBefore: IRedLiveCallbacksStore
+declare const redLiveCallbacksStoreAfter: IRedLiveCallbacksStore
+
+declare const bindFnOnType: (actionType: string, callback: Function, store: IRedLiveCallbacksStore) => void
+
 declare module 'red-live' {
     import { Middleware, Dispatch, AnyAction } from 'redux'
 
-    interface IRedLiveCallbacksStore {
-        [key: string]: Array<Function>
-    }
-
-    const redLiveCallbacksStoreBefore: IRedLiveCallbacksStore
-    const redLiveCallbacksStoreAfter: IRedLiveCallbacksStore
-
-    const bindFnOnType: (actionType: string, callback: Function, store: IRedLiveCallbacksStore) => void
-    
     export const redLiveSubBefore: (actionType: string | Array<string>, callback: Function) => void
     export const redLiveSubAfter: (actionType: string | Array<string>, callback: Function) => void
     export const redLiveMiddleware: Middleware<{}, any, Dispatch<AnyAction>>

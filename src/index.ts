4d6e26f3bed73d7c177b7ecddfd2492f8d5c2a01
Sergey Lutov <doublemakers@gmail.com>
2021-06-25 22:02:42 +0300

    feat(app): install compilation

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
-const redLiveCallbacksStoreBefore = {}
-const redLiveCallbacksStoreAfter = {}
+import { Middleware } from 'redux'
 
-const bindFnOnType = (actionType, callback, store) => {
+interface IRedLiveCallbacksStore {
+    [key: string]: Array<Function>
+}
+
+const redLiveCallbacksStoreBefore: IRedLiveCallbacksStore = {}
+const redLiveCallbacksStoreAfter: IRedLiveCallbacksStore = {}
+
+const bindFnOnType = (actionType: string, callback: Function, store: IRedLiveCallbacksStore) => {
     if (typeof store[actionType] === 'undefined') store[actionType] = []
-
     store[actionType].push(callback)
 }
 
-export const redLiveSubBefore = (actionType, callback) => {
+export const redLiveSubBefore = (actionType: string | Array<string>, callback: Function) => {
     if (Array.isArray(actionType)) {
         actionType.forEach(type => bindFnOnType(type, callback, redLiveCallbacksStoreBefore))
     } else {
@@ -15,7 +20,7 @@ export const redLiveSubBefore = (actionType, callback) => {
     }
 }
 
-export const redLiveSubAfter = (actionType, callback) => {
+export const redLiveSubAfter = (actionType: string | Array<string>, callback: Function) => {
     if (Array.isArray(actionType)) {
         actionType.forEach(type => bindFnOnType(type, callback, redLiveCallbacksStoreAfter))
     } else {
@@ -23,7 +28,7 @@ export const redLiveSubAfter = (actionType, callback) => {
     }
 }
 
-export const redLiveMiddleware = _ => next => action => {
+export const redLive: Middleware = () => next => action => {
     if (typeof redLiveCallbacksStoreBefore[action.type] !== 'undefined') {
         redLiveCallbacksStoreBefore[action.type].forEach(f => f(action))
     }
